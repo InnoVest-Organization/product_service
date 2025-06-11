da50ebfb40a501647d86421222da07b19b84dc52
Laksara K.Y. <129388843+YasiruLaksara@users.noreply.github.com>
2025-06-12 01:09:59 +0530

    add repository method to find inventions by inventor ID

--- src/main/java/com/backend/productservice/repository/InventionRepository.java
+++ src/main/java/com/backend/productservice/repository/InventionRepository.java
@@ -3,9 +3,11 @@ package com.backend.productservice.repository;
 import com.backend.productservice.entity.Invention;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface InventionRepository extends JpaRepository<Invention, Long> {
     Optional<Invention> findByInventionId(Long inventionId);
+    List<Invention> findByInventorId(Long inventorId);
 }
